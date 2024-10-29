2ecc64a5f56da858f279da2a9589de214a5f5679
Daniel Hoffmann <daniel.hoffmann41@gmail.com>
2024-10-29 14:44:31 +0100

    Add bottom row mod.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,7 +1,12 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#define MIRYOKU_LAYER_ALPHAS_COLEMAKDHK
+#define MIRYOKU_LAYER_ALPHAS_COLEMAKDHK \
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
+&kp A,             &kp R,             &kp S,             &kp T,             &kp G,             &kp M,             &kp N,             &kp E,             &kp I,             &kp O,     \
+U_LT(LGUI, Z),     U_MT(LALT, X),     U_MT(LCTRL, C),    U_MT(LSHFT, D),    &kp V,             &kp K,             U_MT(LSHFT, H),    U_MT(LCTRL, COMMA),U_MT(LALT, DOT),   U_LT(LGUI, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
 #define MIRYOKU_CLIPBOARD_WIN
 
 #define MIRYOKU_LAYER_TAP \
